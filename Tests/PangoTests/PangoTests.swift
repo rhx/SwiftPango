c08f8ec306845328a7d4215b691bd6823214563c
Rene Hexel <r.hexel@griffith.edu.au>
2016-08-21 09:29:51 +1000

    Update for Swift 3 preview 6

--- Tests/PangoTests/PangoTests.swift
+++ Tests/PangoTests/PangoTests.swift
@@ -6,7 +6,7 @@ class PangoTests: XCTestCase {
 
     func testPangoColor() {
         var colour = PangoColor();
-        withUnsafeMutablePointer(&colour) {
+        withUnsafeMutablePointer(to: &colour) {
             let red = ColorRef($0);
             XCTAssertTrue(red.parse(spec: "#ff0000"))
             XCTAssertEqual(red.toString(), "#ffff00000000")
@@ -15,7 +15,7 @@ class PangoTests: XCTestCase {
 
     func testPangoColorCopy() {
         var colour = PangoColor();
-        withUnsafeMutablePointer(&colour) {
+        withUnsafeMutablePointer(to: &colour) {
             let one = ColorRef($0);
             XCTAssertTrue(one.parse(spec: "#00ff00"))
             let two = Color(one.copy());
