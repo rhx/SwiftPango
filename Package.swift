bc3022842586d9e06cfba4c8b22e0e120da38df3
Rene Hexel <r.hexel@griffith.edu.au>
2018-12-13 09:45:58 +1000

    Update to Swift 4 package format

--- Package.swift
+++ Package.swift
@@ -1,10 +1,18 @@
+// swift-tools-version:4.0
+
 import PackageDescription
 
 let package = Package(
     name: "Pango",
-    dependencies: [
-        .Package(url: "https://github.com/rhx/CPango.git", majorVersion: 1),
-        .Package(url: "https://github.com/rhx/SwiftGObject.git", majorVersion: 2)
+    products: [
+        .library(name: "Pango", targets: ["Pango"]),
     ],
-    swiftLanguageVersions: [3, 4]
+    dependencies: [
+        .package(url: "https://github.com/rhx/CPango.git", .branch("master")),
+        .package(url: "https://github.com/rhx/SwiftGObject.git", .branch("master"))
+    ],
+    targets: [
+        .target(name: "Pango", dependencies: ["GLibObject"]),
+        .testTarget(name: "PangoTests", dependencies: ["Pango"]),
+    ]
 )
--- gir-to-swift.sh
+++ gir-to-swift.sh
@@ -4,7 +4,7 @@
 # This needs an installed `gir2swift' executable (github.com/rhx/gir2swift)
 #
 . ./config.sh
-mkdir -p Sources
+mkdir -p Sources/${Mod}
 GOBJECT_LIBDIR=`pkg-config --libs-only-L gobject-introspection-1.0 2>/dev/null | tr ' ' '\n' | grep gobject-introspection | tail -n1 | cut -c3-`
 GOBJECT_DIR=`dirname "${GOBJECT_LIBDIR}"`
 for prefix in /usr /usr/local $GOBJECT_DIR $PREFIX ; do
@@ -21,8 +21,8 @@ if [ ! -e "${GIR}" ] ; then
 	echo "and can be found in /usr /usr/local or by pkg-config!"
 	exit 1
 fi
-gir2swift -o Sources -m ${Module}.module -p ${GIR_DIR}/GObject-2.0.gir -p ${GIR_DIR}/GLib-2.0.gir -p ${GIR_DIR}/GObject-2.0.gir "${GIR}"
-for src in Sources/*-*.swift ; do
+gir2swift -o Sources/${Mod} -m ${Module}.module -p ${GIR_DIR}/GObject-2.0.gir -p ${GIR_DIR}/GLib-2.0.gir -p ${GIR_DIR}/GObject-2.0.gir "${GIR}"
+for src in Sources/${Mod}/*-*.swift ; do
 	sed -f ${Module}.sed < ${src} > ${src}.out
 	mv ${src}.out ${src}
 done
