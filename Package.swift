0c80c3765926f8704afebcf9c3e75cbddc5d5eb1
Rene Hexel <r.hexel@griffith.edu.au>
2020-08-31 07:33:06 +1000

    Note Swift 5.2 requirement

--- Package.swift
+++ Package.swift
@@ -1,15 +1,12 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.2
 
 import PackageDescription
 
 let package = Package(
     name: "Pango",
-    products: [
-        .library(name: "Pango", targets: ["Pango"]),
-    ],
+    products: [ .library(name: "Pango", targets: ["Pango"]) ],
     dependencies: [
-        .package(url: "file:///Users/rh/src/swift/rh/gtk/SwiftGObject", .branch("master"))
-        //.package(url: "https://github.com/rhx/SwiftGObject.git", .branch("master"))
+        .package(name: "GLibObject", url: "https://github.com/rhx/SwiftGObject.git", .branch("master"))
     ],
     targets: [
 	.systemLibrary(name: "CPango", pkgConfig: "pango",
