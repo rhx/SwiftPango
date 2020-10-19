a65194bbd07d2e097793ff8b2cf41631ba6cb52c
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-10-20 01:14:02 +0200

    Check build passes

--- Package.swift
+++ Package.swift
@@ -6,10 +6,10 @@ let package = Package(
     name: "Pango",
     products: [ .library(name: "Pango", targets: ["Pango"]) ],
     dependencies: [
-        .package(name: "GLibObject", url: "https://github.com/rhx/SwiftGObject.git", .branch("master"))
+        .package(name: "GLibObject", url: "https://github.com/mikolasstuchlik/SwiftGObject.git", .branch("master"))
     ],
     targets: [
-	.systemLibrary(name: "CPango", pkgConfig: "pango",
+	.systemLibrary(name: "CPango", pkgConfig: "pangocairo", // apperently, `pangoft2` is not required in order to build successfuly
 	    providers: [
 		.brew(["pango", "glib", "glib-networking", "gobject-introspection"]),
 		.apt(["libpango1.0-dev", "libglib2.0-dev", "glib-networking", "gobject-introspection", "libgirepository1.0-dev"])
