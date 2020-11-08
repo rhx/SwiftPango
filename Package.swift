1eecce47da9acfd959b186fdca1bede19cdce315
Mikoláš Stuchlík <stuchlej@gmail.com>
2020-11-08 20:13:02 +0100

    add gir2swift

--- Package.swift
+++ Package.swift
@@ -6,6 +6,7 @@ let package = Package(
     name: "Pango",
     products: [ .library(name: "Pango", targets: ["Pango"]) ],
     dependencies: [
+        .package(name: "gir2swift", url: "https://github.com/mikolasstuchlik/gir2swift.git", .branch("master")),
         .package(name: "GLibObject", url: "https://github.com/mikolasstuchlik/SwiftGObject.git", .branch("master"))
     ],
     targets: [
