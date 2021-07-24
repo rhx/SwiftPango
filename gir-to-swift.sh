b8a35f16b967a9bbf384e97a5c1b895a1816bf2b
Rene Hexel <r.hexel@griffith.edu.au>
2021-07-24 13:30:36 +1000

    Simplify manifest: let gir2swift do sed/awk post-processing

--- gir-to-swift.sh
+++ gir-to-swift.sh
@@ -21,8 +21,4 @@ if [ ! -e "${GIR}" ] ; then
 	echo "and can be found in /usr /usr/local or by pkg-config!"
 	exit 1
 fi
-gir2swift -o Sources/${Mod} -m ${Module}.module -p ${GIR_DIR}/GObject-2.0.gir -p ${GIR_DIR}/GLib-2.0.gir -p ${GIR_DIR}/GObject-2.0.gir "${GIR}"
-for src in Sources/${Mod}/*-*.swift ; do
-	sed -f ${Module}.sed < ${src} > ${src}.out
-	mv ${src}.out ${src}
-done
+gir2swift -o Sources/${Mod} -p ${GIR_DIR}/GObject-2.0.gir -p ${GIR_DIR}/GLib-2.0.gir -p ${GIR_DIR}/GObject-2.0.gir "${GIR}"
